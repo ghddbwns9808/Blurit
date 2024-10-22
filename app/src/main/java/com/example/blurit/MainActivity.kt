e6cdc484ea94aa4782af0a1d0648d19a87b8d687
ghddbwns9808 <ghddbwns9808@gmail.com>
2024-10-22 16:42:17 +0900

    feat: BaseActivity 추가
    MainActivity 가 BaseActivity 상속

--- app/src/main/java/com/example/blurit/MainActivity.kt
+++ app/src/main/java/com/example/blurit/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.blurit.base.BaseActivity
+import com.example.blurit.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    ActivityMainBinding::inflate
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
