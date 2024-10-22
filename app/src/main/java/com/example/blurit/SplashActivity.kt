050c4b27080f23039cceddf504b41b91d4e0718e
ghddbwns9808 <ghddbwns9808@gmail.com>
2024-10-22 15:59:34 +0900

    feat: Splash Activity 기능 구현

--- app/src/main/java/com/example/blurit/SplashActivity.kt
+++ app/src/main/java/com/example/blurit/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.example.blurit
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,11 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
