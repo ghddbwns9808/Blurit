8939c209bd2c60dd837def6552f429b0bfdeb028
ghddbwns9808 <ghddbwns9808@gmail.com>
2024-10-22 20:03:38 +0900

    feat: EditFragment 넘어온 uri logging

---
+++ app/src/main/java/com/example/blurit/edit/EditFragment.kt
@@ -0,0 +1,26 @@
+package com.example.blurit.edit
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.blurit.MainViewModel
+import com.example.blurit.R
+import com.example.blurit.base.BaseFragment
+import com.example.blurit.databinding.FragmentEditBinding
+
+
+private const val TAG = "EditFragment_hong"
+class EditFragment : BaseFragment<FragmentEditBinding>(
+    FragmentEditBinding::bind, R.layout.fragment_edit
+) {
+    private val mainViewModel: MainViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated: ${mainViewModel.getUri()}")
+    }
+}
