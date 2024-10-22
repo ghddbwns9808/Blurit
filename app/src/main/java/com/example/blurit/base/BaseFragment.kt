277209a5944b3f189212337ba818df8534d4a675
ghddbwns9808 <ghddbwns9808@gmail.com>
2024-10-22 16:43:34 +0900

    feat: BaseFragment 추가

---
+++ app/src/main/java/com/example/blurit/base/BaseFragment.kt
@@ -0,0 +1,41 @@
+package com.example.blurit.base
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<B : ViewBinding>(
+    private val bind: (View) -> B,
+    @LayoutRes layoutResId: Int
+) : Fragment(layoutResId) {
+    private var _binding: B? = null
+    protected val binding get() = _binding!!
+    protected lateinit var _activity: Context
+
+    @SuppressLint("ResourceType")
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        _activity = context
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bind(super.onCreateView(inflater, container, savedInstanceState)!!)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
