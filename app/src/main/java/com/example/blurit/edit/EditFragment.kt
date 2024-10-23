c07d8c6fa3674be55230bc2c78833a83b26f6c71
ghddbwns9808 <ghddbwns9808@gmail.com>
2024-10-23 17:24:27 +0900

    feat: EditFragment Click Listener 기능

--- app/src/main/java/com/example/blurit/edit/EditFragment.kt
+++ app/src/main/java/com/example/blurit/edit/EditFragment.kt
@@ -7,10 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.transition.Visibility
 import com.example.blurit.MainViewModel
 import com.example.blurit.R
 import com.example.blurit.base.BaseFragment
 import com.example.blurit.databinding.FragmentEditBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 
 private const val TAG = "EditFragment_hong"
@@ -21,11 +23,37 @@ class EditFragment : BaseFragment<FragmentEditBinding>(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(TAG, "onViewCreated: ${mainViewModel.getUri()}")
+        initView()
+    }
+
+    private fun initView(){
         binding.ivPhoto.setImageURI(mainViewModel.getUri())
 
         binding.tvCancle.setOnClickListener {
             requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        binding.sdBlur.setLabelFormatter{
+            it.toInt().toString()
+        }
+
+        binding.sdThick.setLabelFormatter {
+            it.toInt().toString()
+        }
+
+        binding.ivAuto.setOnClickListener {
+            binding.llBlur.visibility = View.VISIBLE
+            binding.llThick.visibility = View.GONE
+        }
+
+        binding.ivManual.setOnClickListener {
+            binding.llBlur.visibility = View.VISIBLE
+            binding.llThick.visibility = View.VISIBLE
+        }
+
+        binding.ivErase.setOnClickListener {
+            binding.llBlur.visibility = View.GONE
+            binding.llThick.visibility = View.VISIBLE
         }
     }
 }
