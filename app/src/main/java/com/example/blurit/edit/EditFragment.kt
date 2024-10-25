82bb9eb570045b3356e0221914aaace5edb96e5f
ghddbwns9808 <ghddbwns9808@gmail.com>
2024-10-25 17:26:33 +0900

    feat: 두께 설정 기능 구현
    Slider 값 조절 시 두께에 따른 원을 iv_thick_canvas 에 그림
    조절이 끝날 시 eraseThickCircle 함수를 통해 1.5초 후 원 제거

--- app/src/main/java/com/example/blurit/edit/EditFragment.kt
+++ app/src/main/java/com/example/blurit/edit/EditFragment.kt
@@ -1,18 +1,25 @@
 package com.example.blurit.edit
 
+import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.provider.MediaStore
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewTreeObserver
 import androidx.fragment.app.activityViewModels
 import com.example.blurit.MainActivity
 import com.example.blurit.MainViewModel
 import com.example.blurit.R
 import com.example.blurit.base.BaseFragment
 import com.example.blurit.databinding.FragmentEditBinding
+import com.google.android.material.slider.Slider
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
@@ -20,6 +27,7 @@ import com.google.mlkit.vision.face.FaceDetector
 import com.google.mlkit.vision.face.FaceDetectorOptions
 import java.io.IOException
 import kotlin.math.pow
+import kotlin.math.sqrt
 
 private const val TAG = "EditFragment_hong"
 
@@ -37,8 +45,11 @@ class EditFragment : BaseFragment<FragmentEditBinding>(
 
     private lateinit var image: InputImage
     private lateinit var detector: FaceDetector
+
     private lateinit var originalBitmap: Bitmap
-    private lateinit var blurCanvasBitmap: Bitmap
+    private lateinit var blurCanvas: Bitmap
+    private lateinit var thickCanvas: Bitmap
+
     private var mode: EditMode = EditMode.AUTO
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -49,15 +60,7 @@ class EditFragment : BaseFragment<FragmentEditBinding>(
     }
 
     private fun applyAutoMosaic(faces: List<Face>, size: Int) {
-        blurCanvasBitmap = Bitmap.createBitmap(
-            originalBitmap.width,
-            originalBitmap.height,
-            Bitmap.Config.ARGB_8888
-        )
-        binding.ivBlurCanvas.setImageBitmap(blurCanvasBitmap)
-
-        val mosaicBitmap = blurCanvasBitmap.copy(blurCanvasBitmap.config, true)
-        val canvas = Canvas(mosaicBitmap)
+        val canvas = Canvas(blurCanvas)
 
         faces.forEach { face ->
             val boundingBox = face.boundingBox
@@ -68,22 +71,19 @@ class EditFragment : BaseFragment<FragmentEditBinding>(
 
             val blockSize = radius.toInt() / (13 - size)
 
-            // BoundingBox 내에서 모자이크 블록을 적용
+
             for (y in boundingBox.top until boundingBox.bottom step blockSize) {
                 for (x in boundingBox.left until boundingBox.right step blockSize) {
 
-                    // 블록의 중심 좌표 계산
                     val blockCenterX = x + blockSize / 2f
                     val blockCenterY = y + blockSize / 2f
 
-                    // 블록 중심이 원 내부에 있는지 확인
-                    val distanceFromCenter = Math.sqrt(
+                    val distanceFromCenter = sqrt(
                         (blockCenterX - centerX).toDouble().pow(2.0) +
                                 (blockCenterY - centerY).toDouble().pow(2.0)
                     )
 
                     if (distanceFromCenter <= radius) {
-                        // 원 내부에 있을 때만 모자이크 적용
                         val pixelColor = originalBitmap.getPixel(x, y)
 
                         val paint = Paint()
@@ -101,9 +101,16 @@ class EditFragment : BaseFragment<FragmentEditBinding>(
             }
         }
 
-        binding.ivBlurCanvas.setImageBitmap(mosaicBitmap)
+        binding.ivBlurCanvas.setImageBitmap(blurCanvas)
     }
 
+
+    private fun applyManualMosaic(touchX: Int, touchY: Int) {
+
+    }
+
+
+    @SuppressLint("ClickableViewAccessibility")
     private fun initView() {
         binding.ivPhoto.setImageURI(mainViewModel.getUri())
         initBitmap()
@@ -153,27 +160,89 @@ class EditFragment : BaseFragment<FragmentEditBinding>(
                     Log.e(TAG, "Face detection failed: $e")
                 }
         }
-    }
 
-    private fun initBitmap() {
-        try {
-            image = InputImage.fromFilePath(requireContext(), mainViewModel.getUri()!!)
-            originalBitmap = MediaStore.Images.Media.getBitmap(
-                requireContext().contentResolver,
-                mainViewModel.getUri()
-            )
+        binding.sdThick.addOnChangeListener { slider, value, fromUser ->
+            val thickness = value.toInt()
+            showBrushPreview(thickness)
+        }
 
-            blurCanvasBitmap = Bitmap.createBitmap(
-                originalBitmap.width,
-                originalBitmap.height,
-                Bitmap.Config.ARGB_8888
-            )
+        binding.sdThick.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: Slider) {}
+            override fun onStopTrackingTouch(slider: Slider) {
+                eraseThickCircle()
+            }
+        })
 
-            binding.ivBlurCanvas.setImageBitmap(blurCanvasBitmap)
-        } catch (e: IOException) {
-            e.printStackTrace()
+        binding.ivBlurCanvas.setOnTouchListener { v, event ->
+            if (mode == EditMode.MANUAL) {
+                if (event.action == MotionEvent.ACTION_DOWN || event.action == MotionEvent.ACTION_MOVE) {
+                    applyManualMosaic(event.x.toInt(), event.y.toInt())
+                }
+            }
+            true
         }
     }
+
+    private fun showBrushPreview(thickness: Int) {
+        val rad = thickness + binding.ivThickCanvas.width / 30
+
+        val thickBitmap = thickCanvas.copy(thickCanvas.config, true)
+        val canvas = Canvas(thickBitmap)
+        val paint = Paint().apply {
+            color = Color.argb(160, 100, 100, 100)
+            style = Paint.Style.FILL
+        }
+
+        val centerX = canvas.width / 2f
+        val centerY = canvas.height / 2f
+
+        canvas.drawCircle(centerX, centerY, rad.toFloat(), paint)
+        binding.ivThickCanvas.setImageBitmap(thickBitmap)
+    }
+
+    private fun eraseThickCircle() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val emptyBitmap = thickCanvas.copy(thickCanvas.config, true)
+            binding.ivThickCanvas.setImageBitmap(emptyBitmap)
+        }, 1500)
+    }
+
+
+    private fun initBitmap() {
+        binding.flCanvas.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                // 뷰의 레이아웃이 완료된 후 크기를 얻을 수 있습니다.
+                binding.flCanvas.viewTreeObserver.removeOnGlobalLayoutListener(this)  // 리스너 제거
+
+                try {
+                    image = InputImage.fromFilePath(requireContext(), mainViewModel.getUri()!!)
+                    originalBitmap = MediaStore.Images.Media.getBitmap(
+                        requireContext().contentResolver,
+                        mainViewModel.getUri()
+                    )
+
+                    // 이제 뷰의 크기를 사용할 수 있음
+                    blurCanvas = Bitmap.createBitmap(
+                        originalBitmap.width,
+                        originalBitmap.height,
+                        Bitmap.Config.ARGB_8888
+                    )
+
+                    thickCanvas = Bitmap.createBitmap(
+                        binding.ivThickCanvas.width,
+                        binding.ivThickCanvas.height,
+                        Bitmap.Config.ARGB_8888
+                    )
+
+                    binding.ivBlurCanvas.setImageBitmap(blurCanvas)
+                    binding.ivThickCanvas.setImageBitmap(thickCanvas)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        })
+    }
+
 
     enum class EditMode {
         AUTO, MANUAL, ERASE
