eacde5af4afa611fdfff0f38478888b3297b9cc8
ghddbwns9808 <ghddbwns9808@gmail.com>
2024-10-28 22:42:44 +0900

    feat: HomeFragment에서 뒤로가기 두번 눌러 앱 종료 기능

--- app/src/main/java/com/example/blurit/home/HomeFragment.kt
+++ app/src/main/java/com/example/blurit/home/HomeFragment.kt
@@ -1,11 +1,16 @@
 package com.example.blurit.home
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
+import androidx.activity.addCallback
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.fragment.app.activityViewModels
+import com.example.blurit.MainActivity
 import com.example.blurit.MainViewModel
 import com.example.blurit.R
 import com.example.blurit.base.BaseFragment
@@ -18,6 +23,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
     FragmentHomeBinding::bind, R.layout.fragment_home
 ) {
     private val mainViewModel: MainViewModel by activityViewModels()
+    private lateinit var activity: MainActivity
+    private var backPressedOnce = false
 
     private val pickMedia = registerForActivityResult(PickVisualMedia()) { uri ->
         if (uri != null) {
@@ -35,9 +42,25 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        activity = _activity as MainActivity
 
         binding.ivPhoto.setOnClickListener {
             pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
         }
+
+        activity.onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            backPressToQuit()
+        }
+    }
+
+    private fun backPressToQuit() {
+        if (backPressedOnce) {
+            requireActivity().finish()
+            return
+        }
+        backPressedOnce = true
+        activity.showToast(activity.getString(R.string.backpress_to_quit))
+
+        Handler(Looper.getMainLooper()).postDelayed({ backPressedOnce = false }, 2000)
     }
 }
