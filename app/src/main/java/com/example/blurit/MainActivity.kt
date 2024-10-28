795ecce8cf914b3adc2187ee7ab26165945c432d
ghddbwns9808 <ghddbwns9808@gmail.com>
2024-10-28 15:12:02 +0900

    feat: 외부 앱에서 공유하기에 Blurit 앱 옵션에 표출
    넘어온 Image의 Uri를 바로 EditFragment 에 넘기기

--- app/src/main/java/com/example/blurit/MainActivity.kt
+++ app/src/main/java/com/example/blurit/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.blurit
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.blurit.base.BaseActivity
 import com.example.blurit.databinding.ActivityMainBinding
+import com.example.blurit.edit.EditFragment
 import com.example.blurit.home.HomeFragment
 
 class MainActivity : BaseActivity<ActivityMainBinding>(
@@ -14,6 +19,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
 ) {
     private val mainViewModel: MainViewModel by viewModels()
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,6 +33,19 @@ class MainActivity : BaseActivity<ActivityMainBinding>(
         supportFragmentManager.beginTransaction()
             .replace(R.id.layout_main_fragment, HomeFragment())
             .commit()
+
+        val action: String = intent.action ?:""
+        val type: String? = intent.type
+
+        if (Intent.ACTION_SEND == action && type != null) {
+            if ("image/*" == type) {
+                val imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+                mainViewModel.setUri(imageUri)
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.layout_main_fragment, EditFragment())
+                    .commit()
+            }
+        }
 
     }
 }
